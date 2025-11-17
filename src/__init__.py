687d155c302766751d854a9033cb8f571aaef220
민성 Skystarry <vmintf@gmail.com>
2025-11-17 19:28:44 +0900

    chore: Correct author name in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ except (ImportError, ModuleNotFoundError):
     raise ImportError("PyTorch is required to use WsFFN.\nPlease install it using: `pip install torch`")
 
 __version__ = "0.0.1"
-__author__ = "민성 SKystarry"
+__author__ = "민성 Skystarry"
 
 from .WsFFN import wsFFN, Config
 
